c82ebac9784dcc2513b896cbabbd03ced55fb82d
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-03-30 23:12:47 +0100

    fix fotogallery page link

---
+++ src/pages/fotogallery.jsx
@@ -0,0 +1,9 @@
+const Fotogallery = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      Hello from Fotogallery
+    </main>
+  );
+}
+
+export default Fotogallery
