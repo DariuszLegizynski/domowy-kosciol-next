b5cdd332906b9f490bc6db463743e2565f1c1956
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-09 12:37:33 +0200

    add under construction pages info

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,11 +2,10 @@ import BackButton from "@/components/base/BackButton"
 
 const About = () => {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<article>
-				<h1>W Budowie</h1>
-				<BackButton />
-			</article>
+		<main className="flex min-h-screen flex-col items-center justify-center">
+			<h1 className="pb-4">W Budowie</h1>
+			<h2>(under construction)</h2>
+			<BackButton />
 		</main>
 	)
 }
