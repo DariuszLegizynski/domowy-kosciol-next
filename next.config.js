b9d443656281974f3a72d263201fd6cbdbef09cc
Dariusz Legizynski <d.legizynski@.com>
2024-05-28 12:22:09 +0200

    add more domains for Image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["http://89.58.8.55/", "localhost"],
+		domains: ["http://89.58.8.55/", "localhost", "http://89.58.8.55:1337", "89.58.8.55"],
 	},
 }
 
