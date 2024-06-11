e0850be143c304999f4550507ef69b6cbac4c049
Dariusz Legizynski <d.legizynski@.com>
2024-06-11 21:59:53 +0200

    add subpages about icon and testimonials

---
+++ src/components/about/TestimonialPage.tsx
@@ -0,0 +1,47 @@
+const TestimonialPage = () => {
+	return (
+		<article className="grid gap-y-8 px-4">
+			<section>
+				<div className="h2 pb-2">Świadectwo 1</div>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, pariatur. Fuga eum illum dignissimos error, aperiam totam aut non, possimus atque
+					tempore voluptatem, ab sint magni delectus necessitatibus. Accusamus, rerum? Error labore necessitatibus odio quam optio sint magni dolores quo
+					excepturi veritatis id accusamus eaque velit possimus recusandae, tempore doloribus deleniti placeat ducimus quos iste saepe. Deserunt eaque
+					repudiandae accusantium. Laborum praesentium alias sit possimus nam voluptatum sequi temporibus qui voluptas inventore, eligendi repellat ullam
+					dolores. Pariatur fugit, soluta debitis quam consequuntur quae similique reiciendis facilis temporibus vitae est rem. Eum amet consequuntur tempore
+					asperiores iusto cumque, doloremque culpa beatae illum est dicta adipisci odit deserunt omnis expedita debitis sunt voluptatibus repellendus maiores
+					placeat? Eaque aspernatur ipsam assumenda blanditiis facilis. Reiciendis perferendis quia neque, sit corrupti aspernatur, nam iure, facilis blanditiis
+					tempora consequuntur eos eveniet quae? Ipsum illo quam iste? Eius, ratione impedit officiis ipsum nam vitae perspiciatis pariatur cumque.
+				</p>
+			</section>
+			<section>
+				<div className="h2 pb-2">Świadectwo 2</div>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, pariatur. Fuga eum illum dignissimos error, aperiam totam aut non, possimus atque
+					tempore voluptatem, ab sint magni delectus necessitatibus. Accusamus, rerum? Error labore necessitatibus odio quam optio sint magni dolores quo
+					excepturi veritatis id accusamus eaque velit possimus recusandae, tempore doloribus deleniti placeat ducimus quos iste saepe. Deserunt eaque
+					repudiandae accusantium. Laborum praesentium alias sit possimus nam voluptatum sequi temporibus qui voluptas inventore, eligendi repellat ullam
+					dolores. Pariatur fugit, soluta debitis quam consequuntur quae similique reiciendis facilis temporibus vitae est rem. Eum amet consequuntur tempore
+					asperiores iusto cumque, doloremque culpa beatae illum est dicta adipisci odit deserunt omnis expedita debitis sunt voluptatibus repellendus maiores
+					placeat? Eaque aspernatur ipsam assumenda blanditiis facilis. Reiciendis perferendis quia neque, sit corrupti aspernatur, nam iure, facilis blanditiis
+					tempora consequuntur eos eveniet quae? Ipsum illo quam iste? Eius, ratione impedit officiis ipsum nam vitae perspiciatis pariatur cumque.
+				</p>
+			</section>
+			<section>
+				<div className="h2 pb-2">Świadectwo 3</div>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, pariatur. Fuga eum illum dignissimos error, aperiam totam aut non, possimus atque
+					tempore voluptatem, ab sint magni delectus necessitatibus. Accusamus, rerum? Error labore necessitatibus odio quam optio sint magni dolores quo
+					excepturi veritatis id accusamus eaque velit possimus recusandae, tempore doloribus deleniti placeat ducimus quos iste saepe. Deserunt eaque
+					repudiandae accusantium. Laborum praesentium alias sit possimus nam voluptatum sequi temporibus qui voluptas inventore, eligendi repellat ullam
+					dolores. Pariatur fugit, soluta debitis quam consequuntur quae similique reiciendis facilis temporibus vitae est rem. Eum amet consequuntur tempore
+					asperiores iusto cumque, doloremque culpa beatae illum est dicta adipisci odit deserunt omnis expedita debitis sunt voluptatibus repellendus maiores
+					placeat? Eaque aspernatur ipsam assumenda blanditiis facilis. Reiciendis perferendis quia neque, sit corrupti aspernatur, nam iure, facilis blanditiis
+					tempora consequuntur eos eveniet quae? Ipsum illo quam iste? Eius, ratione impedit officiis ipsum nam vitae perspiciatis pariatur cumque.
+				</p>
+			</section>
+		</article>
+	)
+}
+
+export default TestimonialPage
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,15 @@
-import BackButton from "@/components/base/BackButton"
-import Image from "next/image"
+import { useState } from "react"
+
+import AboutPage from "@/components/about/AboutPage"
+import IconPage from "@/components/about/IconPage"
+import TestimonialPage from "@/components/about/TestimonialPage"
 
 import Layout from "../components/Layout"
+import BackButton from "@/components/base/BackButton"
 
 const About = () => {
+	const [step, setStep] = useState(1)
+
 	return (
 		<Layout>
 			<main className="flex min-h-screen flex-col items-center justify-center">
@@ -11,48 +17,14 @@ const About = () => {
 					<h1 className="text-start text-primary pt-2 pl-8">O nas,</h1>
 					<h1 className="text-center text-white pb-2">czyli kim właściwie jesteśmy?</h1>
 				</section>
-
-				<article className="mt-16 px-4 pb-4 grid gap-y-2">
-					<section>
-						<Image className="py-8" src="/images/about/picture-1.jpg" alt="Happy family" height={400} width={600} />
-						<div className="grid gap-y-2">
-							<h2 className="text-primary text-[3rem] leading-10 pb-2">Kochane małżeństwa!</h2>
-							<p>Macie ochotę na duchową przygodę, która wciągnie Was po uszy?</p>
-							<p>Będzie radośnie, będzie z przytupem i na pewno zabawnie!</p>
-							<p>U nas nie zabraknie rozmów i mega okazji do rozwiązywania własnych problemów z pomocą Bożej łaski płynącej z Sakramentu Małżeństwa.</p>
-						</div>
-					</section>
-
-					<section>
-						<Image className="py-8" src="/images/about/jed-villejo-4SByp8kIoOE-unsplash.jpg" alt="Happy family" height={400} width={600} />
-						<div className="grid gap-y-2">
-							<p>
-								Poznajcie nas - Domowy Kościół w Austrii przy Polskiej Misji Katolickiej Księży Zmartwychwstańców w Wiedniu – miejsce, gdzie Wasz dom staje się
-								„małym Kościołem” i sercem wspaniałej wspólnoty!
-							</p>
-							<p>
-								Spotykamy się raz w miesiącu, tworząc tzw kręgi, czyli grupy małżeńskie, żeby być bliżej Boga i siebie nawzajem. Każde spotkanie jest jak wielka
-								duchowa impreza, uczestniczy w nich też nasz ulubiony kapłan – duchowy doradca.
-							</p>
-						</div>
-					</section>
-
-					<section>
-						<Image className="py-8" src="/images/about/paz-arando-qZIM5rutBZM-unsplash.jpg" alt="Happy family" height={400} width={600} />
-						<div className="grid gap-y-2">
-							<p>
-								Dwa razy w roku wskakujemy w rekolekcyjne buty i ruszamy na weekendowe wypady, prowadzone przez kapłana i mega inspirującą parę małżeńską z
-								Polski.
-							</p>
-							<p>A gdy robi się cieplej, włączamy tryb piknikowy i robimy fajne wycieczki na świeżym powietrzu.</p>
-							<p>Domowy Kościół w Austrii to część wielkiej rodziny Ruchu Światło-Życie, który zaczynał w Polsce i dzięki emigrantom teraz podbija świat.</p>
-							<p>
-								Dołącz do nas i przeżyj niesamowitą przygodę, poznaj prawdziwego Ojca, Jego Syna, i Ducha, który także jest święty. Zobacz, jak wiele radości
-								może przynieść wspólna wiara i zabawa!
-							</p>
-						</div>
-					</section>
-				</article>
+				<section className="flex gap-x-8 py-16">
+					<button onClick={() => setStep(1)}>O Nas</button>
+					<button onClick={() => setStep(2)}>Nasza Ikona</button>
+					<button onClick={() => setStep(3)}>Świadectwa</button>
+				</section>
+				{step === 1 && <AboutPage />}
+				{step === 2 && <IconPage />}
+				{step === 3 && <TestimonialPage />}
 				<BackButton />
 			</main>
 		</Layout>
