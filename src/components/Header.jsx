6ec40b5b0e51382fd3cc0b911d61151eeb496544
Dariusz Legizynski <d.legizynski@.com>
2024-06-13 08:42:23 +0200

    remove cross icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ import BaseButton from "@/components/base/BaseButton"
 const Header = ({ isActive = false }) => {
 	return (
 		<header className={`${isActive ? "left-1/2 transform -translate-x-1/2 w-full sm:w-[60vw]" : "w-full"} fixed h-12 z-10 backdrop-blur-md`}>
-			<section className="max-w-[62rem] grid grid-cols-[1fr_auto_1fr] h-12 items-center justify-items-center px-4 mx-auto fadeInFromTop">
+			<section className="max-w-[72vw] grid grid-cols-[auto_auto] h-12 items-center justify-self-center mx-auto fadeInFromTop">
 				<Logo />
-				<IntroButton />
+				{/* <IntroButton /> */}
 				<Link className={`${isActive ? "text-white" : "text-black pr-2"} justify-self-end`} href="/contact">
 					<BaseButton text="Kontakt" />
 				</Link>
