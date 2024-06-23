62bdffad348b23ab2e660db6fb2d5518793a9a4e
Dariusz Legizynski <d.legizynski@.com>
2024-06-23 20:34:44 +0200

    add form validation

--- src/components/base/BaseText.tsx
+++ src/components/base/BaseText.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react"
+import { useState, FocusEvent } from "react"
 
 interface BaseTextProps {
 	name: string
 	text: string
 	inputFieldType: string
 	isRequired?: boolean
+	field?: {
+		onBlur?: (e: FocusEvent<HTMLInputElement>) => void
+		onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+		value?: string
+	}
+	form?: any
 }
 
-const BaseText: React.FC<BaseTextProps> = ({ name, text, inputFieldType = "text", isRequired = false }) => {
+const BaseText: React.FC<BaseTextProps> = ({ name, text, inputFieldType = "text", isRequired = false, field = {} }) => {
 	const [isActive, setIsActive] = useState(false)
 
 	const handleFocus = () => {
 		setIsActive(true)
 	}
 
-	const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+	const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
 		if (e.target.value === "") {
 			setIsActive(false)
 		}
+		field.onBlur?.(e)
 	}
+
 	return (
 		<div className="relative max-w-full">
 			<label className={`absolute bottom-0 left-1 transition-all duration-500 ease-in-out -translate-y-full text-sm ${isActive ? "opacity-100" : "opacity-0"}`}>
@@ -30,9 +38,10 @@ const BaseText: React.FC<BaseTextProps> = ({ name, text, inputFieldType = "text"
 				{isRequired ? "*" : ""}
 			</span>
 			<input
+				{...field}
 				onFocus={handleFocus}
 				onBlur={handleBlur}
-				className={`pl-1 w-full border-b border-priamry transition-all duration-300 ease-in-out bg-transparent`}
+				className={`pl-1 w-full border-b border-primary transition-all duration-300 ease-in-out bg-transparent`}
 				name={name}
 				type={inputFieldType}
 				required={isRequired}
